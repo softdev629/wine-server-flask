d836f94c5e1c5c58d599c066d340077579ec5b63
violetstar701 <violetstar701@gmail.com>
2023-03-27 04:51:59 +0300

    winde name repeat avoid, link, gpt-4 upgrade

--- app.py
+++ app.py
@@ -1,40 +1,53 @@
+import dotenv
 from flask import Flask, request
 from flask_cors import CORS
+from dotenv import load_dotenv
+import pandas as pd
+from pandas import DataFrame
 from langchain.embeddings.openai import OpenAIEmbeddings
-# from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import TextLoader
+from langchain.docstore.document import Document
+from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
+from langchain import OpenAI, PromptTemplate, FAISS
 from langchain.chains.conversation.memory import ConversationBufferMemory
 
-# with open("./wineinfo.txt") as f:
-#     state_of_the_union = f.read()
-# text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-# texts = text_splitter.split_text(state_of_the_union)
+load_dotenv()
+
+loader = TextLoader('./wineinfo.txt')
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
 
 embeddings = OpenAIEmbeddings()
-# docsearch = FAISS.from_texts(texts, embeddings, metadatas=[{"source": i} for i in range(len(texts))])
-# docsearch.save_local("./embeddings")
-docsearch = FAISS.load_local("./embeddings", embeddings)
-# print("complete")
+# vector_db = FAISS.from_documents(docs, embeddings)
+# vector_db.save_local("./embeddings")
+vector_db = FAISS.load_local("./embeddings", embeddings)
+llm = ChatOpenAI(temperature=0)
 
-template = """You are a chatbot having a conversation with a human
+# extra_info: DataFrame = pd.read_excel("qa.xlsx")
+# qa_list = extra_info.to_dict(orient="records")
+# for qa_item in qa_list:
+#     document = Document(page_content=f"Q: {qa_item['Prompt']}\nA: {qa_item['Completion']}")
+#     vector_db.add_documents([document])
 
+vector_template = """You are a chatbot having a conversation with a human
+
+Try to avoid repeating one wine name several times and if possible try to mention product url everytime.
 Given the following extracted parts of a long document and a question, create a final answer.
 
 {context}
 
-{chat_history}
 Human: {human_input}
 Chatbot:"""
 
-prompt = PromptTemplate(
-    input_variables=["chat_history", "human_input", "context"],
-    template=template
+vector_prompt = PromptTemplate(
+    input_variables=["human_input", "context"],
+    template=vector_template
 )
 memory = ConversationBufferMemory(memory_key="chat_history", input_key="human_input")
-chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff", memory=memory, prompt=prompt)
+vector_chain = load_qa_chain(llm=llm, chain_type="stuff", prompt=vector_prompt, memory=memory)
 
 app = Flask(__name__)
 CORS(app)
@@ -42,7 +55,10 @@ CORS(app)
 @app.route('/mad', methods=['POST'])
 def mad():
     query = request.form["prompt"]
-    docs = docsearch.similarity_search(query)
-    output = chain({"input_documents": docs, "human_input": query}, return_only_outputs=True)
-    return {"answer": output["output_text"]}
-    # return {"answer": "answer"}
+    docs = vector_db.similarity_search(query)
+    vector_output = vector_chain({"input_documents": docs, "human_input": query}, return_only_outputs=True)
+
+    return {"answer": vector_output["output_text"]}
+
+if __name__ == '__main__':
+    app.run(debug=True)
